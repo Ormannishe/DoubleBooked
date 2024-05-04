7dec18a376ce526eeac36604501e69d124231ffa
Ormannishe <james.roose@hotmail.com>
2024-05-04 15:49:52 -0400

    Add tests and fix bug comparing nil intervals

--- src/double_booked/core.clj
+++ src/double_booked/core.clj
@@ -1,39 +1,9 @@
-(ns double-booked.core
-  (:require [clj-time.core :as t]
-            [clj-time.format :as f]))
-
-;; Double Booked
-;;
 ;; When maintaining a calendar of events, it is important to know if an event overlaps with another event.
 ;; Given a sequence of events, each having a start and end time,
 ;; write a program that will return the sequence of all pairs of overlapping events.
-
-;; TODO: Put these into proper tests
-(def sample-events
-  [{:id 1
-    :title "Litera All Staff Meeting"
-    :start "2024-05-03T10:00:00.000Z"
-    :end "2024-05-03T11:00:00.000Z"}
-   {:id 2
-    :title "James / Karen BiWeekly 1:1"
-    :start "2024-05-03T10:15:00.000Z"
-    :end "2024-05-03T11:45:00.000Z"}
-   {:id 3
-    :title "OOO Dentist"
-    :start "2024-05-03T00:00:00.000Z"
-    :end "2024-05-04T00:00:00.000Z"}
-   {:id 4
-    :title "Quick Sync Up"
-    :start "2024-05-03T10:00:00.000Z"
-    :end "2024-05-03T10:15:00.000Z"}
-   {:id 5
-    :title "Lunch"
-    :start "2024-05-03T11:00:00.000Z"
-    :end "2024-05-03T12:00:00.000Z"}
-   {:id 6
-    :title "Cottage Weekend"
-    :start "2024-05-06T00:00:00.000Z"
-    :end "2024-05-08T00:00:00.000Z"}])
+(ns double-booked.core
+  (:require [clj-time.core :as t]
+            [clj-time.format :as f]))
 
 (defn- make-interval
   "Given an event with a start and end time, return a clj-time interval."
@@ -44,9 +14,10 @@
 (defn- conflicts?
   "Given two events, returns true if their start and end times overlap, and false otherwise."
   [event1 event2]
-  (when (and event1 event2)
-    (t/overlaps? (make-interval event1)
-                 (make-interval event2))))
+  (let [interval1 (make-interval event1)
+        interval2 (make-interval event2)]
+    (when (and interval1 interval2)
+      (t/overlaps? interval1 interval2))))
 
 ;; TODO: Might as well implement the more efficient solution as well ?
 ;;
@@ -56,8 +27,7 @@
 (defn find-conflicts
   "Given a list of events, return a list of all pairs of events with overlaping times."
   ([events]
-   (when (seq events)
-     (find-conflicts (first events) (rest events))))
+   (find-conflicts (first events) (rest events)))
   ([current-event upcoming-events]
    (when (seq upcoming-events)
      (concat (keep (fn [next-event]
--- test/double_booked/core_test.clj
+++ test/double_booked/core_test.clj
@@ -1,7 +1,73 @@
 (ns double-booked.core-test
-  (:require [clojure.test :refer :all]
-            [double-booked.core :refer :all]))
+  (:require [clojure.test :refer [deftest testing is]]
+            [double-booked.core :as core]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def events-with-missing-data
+  [{:id 1
+    :title "The Rapture"
+    :start "2029-11-01T00:00:00.000Z"}
+   {:id 2
+    :title "The End Of The World"
+    :end "2029-12-31T23:59:59.999Z"}
+   {:id 3
+    :title "Generate New Universe" ;; This takes a full year :)
+    :start "2029-01-01T00:00:00.000Z"
+    :end "2030-01-01T00:00:00.000Z"}])
+
+(def back-to-back-events
+  [{:id 1
+    :title "May 5th"
+    :start "2024-05-05T00:00:00.000Z"
+    :end "2024-05-06T00:00:00.000Z"}
+   {:id 2
+    :title "May 6th"
+    :start "2024-05-06T00:00:00.000Z"
+    :end "2024-05-07T00:00:00.000Z"}])
+
+;; Contains several overlapping events, some nested
+(def overlapping-events
+  [{:id 1
+    :title "Litera All Staff Meeting"
+    :start "2024-05-03T10:00:00.000Z"
+    :end "2024-05-03T11:00:00.000Z"}
+   {:id 2
+    :title "James / Karen BiWeekly 1:1"
+    :start "2024-05-03T10:15:00.000Z"
+    :end "2024-05-03T11:45:00.000Z"}
+   {:id 3
+    :title "OOO Dentist"
+    :start "2024-05-03T00:00:00.000Z"
+    :end "2024-05-04T00:00:00.000Z"}
+   {:id 4
+    :title "Quick Sync Up"
+    :start "2024-05-03T10:00:00.000Z"
+    :end "2024-05-03T10:15:00.000Z"}
+   {:id 5
+    :title "Lunch"
+    :start "2024-05-03T11:00:00.000Z"
+    :end "2024-05-03T12:00:00.000Z"}
+   {:id 6
+    :title "Cottage Weekend"
+    :start "2024-05-06T00:00:00.000Z"
+    :end "2024-05-08T00:00:00.000Z"}])
+
+(defn pairs->IDs
+  "Given a list of paired overlapping events, extract just the IDs in each pair for easier comparison."
+  [pairs]
+  (map (partial map :id) pairs))
+
+(deftest find-conflicts-test
+  ;; I thought about ensuring the function always returns a list as described in its docstring
+  ;; but in general I prefer when functions return nil when given invalid input and no exception handling is required
+  (testing "returns nil when given nil or an empty list"
+    (is (nil? (core/find-conflicts nil)))
+    (is (nil? (core/find-conflicts '()))))
+  (testing "events that are missing a start or end date are not considered"
+    (is (empty? (core/find-conflicts events-with-missing-data))))
+  (testing "events which end and start at the exact same time don't actually overlap"
+    (is (empty? (core/find-conflicts back-to-back-events))))
+  ;; For ease of comparison I've added IDs to the events so we can just compare the set of conflicting IDs instead of the whole event
+  (testing "All pairs of overlapping events are returned"
+    (let [conflicting-IDs `((1 2) (1 3) (1 4) (2 3) (2 5) (3 4) (3 5))]
+      (is (= (pairs->IDs (core/find-conflicts overlapping-events))
+             conflicting-IDs)))))
